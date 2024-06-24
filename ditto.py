00afd0deaf3957a9c3e04761c6c0fbd1fd4697d7
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2024-06-24 11:03:47 -0300

    Formatando o json

--- ditto.py
+++ ditto.py
@@ -4,8 +4,8 @@ import json
 
 resposta = requests.get('https://pokeapi.co/api/v2/pokemon/ditto')
 dados = resposta.json()
-dados_json = json.dumps(dados)
+dados_json = json.dumps(dados, indent=4)
 
-with open('/home/navarro/projects/pokeapi/ditto.json', 'w') as arquivo:
+with open('/home/navarro/lakehouse/ditto.json', 'w') as arquivo:
     arquivo.write(dados_json)
     print('Pegamos as informações do Ditto!')
