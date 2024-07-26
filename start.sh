595f0b63aa86220382947e19e44017da4ff0f18f
eduardoKatsurayama <58867723+eduardoKatsurayama@users.noreply.github.com>
2024-07-25 21:27:31 -0300

    Revert "Subindo yarn e adicionando arquivo ditto no HDFS (#3)" (#8)
    This reverts commit b915503e35489625369f65800fa51bc4f5ca3ffd.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker build -t hadoop_spark .
-docker run --rm -it -p 9870:9870 -p 8088:8088 hadoop_spark
+docker build -t test_spark .
+docker run --rm -it -p 9870:9870 test_spark
